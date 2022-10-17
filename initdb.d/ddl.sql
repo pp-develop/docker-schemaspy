89a3ce788480df0efbf3872c025e18d999e90e22
pp <ichikawa929@yahoo.co.jp>
2022-10-18 00:15:54 +0900

    fix: update ddl

--- initdb.d/ddl.sql
+++ initdb.d/ddl.sql
@@ -1,6 +1,8 @@
+use database;
+
 create table `groups`(
     `group_id` int not null auto_increment primary key,
-    `group_name` VARCHAR(50),
+    `group_name` VARCHAR(50)
 );
 
 create table users(
